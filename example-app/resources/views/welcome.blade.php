93695fba577fcc43cde068f6b0507b6385b8d1d5
lolzazayou9 <65160227@go.buu.ac.th>
2024-02-05 19:23:44 +0700

    javasc

--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -1,10 +1,59 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Javascript 101</title>
+        <title>JAVA Script 101 </title>
+        
     </head>
-    <body>
-        <h1>Javascript</h1>
+    <body >
+        <h1 id="myh1">JAVAScript</h1>
+        <button onclick="alert('bug hum noye')">Click me ummmm</button>
+        <input type="text" id="my_number" >
+        <button onclick="myFunction()">submit number</button>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                console.log("Hello World - document.ready")
+                console.log($('#myh1').text())
+                console.log($('#my_number').val())
+                $('#myh1').text("Javascript 101")
+                $('#my_number').val(100)
+            });
+            function myFunction(){
+                let my_number = parseInt($('#my_number').val())
+                for(let i = 0 ; i < my_number;i++){
+                    $('#myh1').after(`<h1 class="my_gen_number">${i}</h1>`)
+                }
+                console.log(document.getElementById('my_number').value);
+                console.log('Click submit number')
+                console.log('Click submit number')
+            }
+            console.log("Hum")
+            let maval;
+            maval = '10';
+            maval2 ='2';
+            console.log(maval,maval2);
+            maval3=parseInt(maval)+parseInt(maval2);
+            console.log(maval3);
+            maval3=maval-maval2;
+            console.log(maval3);
+            maval3=maval*maval2;
+            console.log(maval3);
+            maval3=maval/maval2;
+            console.log(maval3);
+
+            let maval4 = [1,2,3];
+            maval4[3]=5;
+            console.log(maval4);
+            maval4[4]=[9,8,5];
+            console.log(maval4);
+
+            for(i = 0 ; i < maval4.length;i++){
+                console.log(maval4[i])
+            }
+            maval4.forEach(function(value, index){
+            console.log("in forEach",value,index)});
+
+           console.log(document.getElementById('myh1').innerHTML);
+        </script>
     </body>
-    
-        </html>
+</html>
