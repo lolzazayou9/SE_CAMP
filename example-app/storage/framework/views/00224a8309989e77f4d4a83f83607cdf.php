20f692620d21303cb3649503032cf5f46cbd6089
lolzazayou9 <65160227@go.buu.ac.th>
2024-02-12 17:50:53 +0700

    javascript*

---
+++ example-app/storage/framework/views/00224a8309989e77f4d4a83f83607cdf.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+<body>
+    <h1> สูตรคูณ </h1>
+<form method="post" action="/my-route">
+    <?php echo csrf_field(); ?>
+    <input type="text" name="myinput">
+    <button type="submit">Submit</button>
+</form>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\SE_CAMP\example-app\resources\views/myFolder/mypage.blade.php ENDPATH**/ ?>
---
+++ example-app/storage/framework/views/df93727d0ce33ef4644c402cba82c38d.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JAVA Script 101 </title>
+        
+    </head>
+    <body >
+        <h1 id="myh1">แม่สูตรคูณ</h1>
+        <button onclick="alert('bug hum noye')">Click me ummmm</button>
+        <input type="text" id="my_number" >
+        <button onclick="myFunction()">submit number</button>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                console.log("Hello World - document.ready")
+                console.log($('#myh1').text())
+                console.log($('#my_number').val())
+                
+                
+            });
+            function myFunction(){
+                let my_number = parseInt($('#my_number').val())
+                for(let i = 1 ; i <= 12;i++){
+                    let re = i * my_number;
+                    $('#myh1').after(`<h1 class="my_gen_number">${my_number} * ${i} = ${re}</h1>`)
+                }
+            }
+            console.log("Hum")
+            let maval;
+            maval = '10';
+            maval2 ='2';
+            console.log(maval,maval2);
+            maval3=parseInt(maval)+parseInt(maval2);
+            console.log(maval3);
+            maval3=maval-maval2;
+            console.log(maval3);
+            maval3=maval*maval2;
+            console.log(maval3);
+            maval3=maval/maval2;
+            console.log(maval3);
+
+            let maval4 = [1,2,3];
+            maval4[3]=5;
+            console.log(maval4);
+            maval4[4]=[9,8,5];
+            console.log(maval4);
+
+            for(i = 0 ; i < maval4.length;i++){
+                console.log(maval4[i])
+            }
+            maval4.forEach(function(value, index){
+            console.log("in forEach",value,index)});
+
+           console.log(document.getElementById('myh1').innerHTML);
+        </script>
+    </body>
+</html>
+<?php /**PATH C:\xampp\htdocs\SE_CAMP\example-app\resources\views/welcome.blade.php ENDPATH**/ ?>
